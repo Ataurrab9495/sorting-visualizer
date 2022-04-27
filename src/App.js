64922e0294ca952792518e298d8496af6d612d8d
Sasanka <mohammedataurrab@gmail.com>
2022-04-27 11:06:41 +0530

    initial commit 2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React,{useState} from 'react';
 import { Container } from 'react-bootstrap';
 import './App.css';
 import Visualizer from './component/visualizer';
 
 function App() {
+  const [input , setInput] = useState(5);
+
+  const range = () =>{
+    const rangeValue = prompt("Enter the Range:-");
+    setInput(rangeValue)
+  }
   return (
-    <Container className='d-flex justify-content-center' style={{height:'100vh'}}>
-      <div className='w-100'>
-      <Visualizer/>
+    <Container className='d-flex align-items-center justify-content-end' style={{height:'100vh'}} fluid>
+      <div className='w-100' style={{maxWidth:'100%'}}>
+      <Visualizer range={range} input={input}/>
       </div>
+      <p>{input}</p>
     </Container>
   );
 }
--- src/component/visualizer.js
+++ src/component/visualizer.js
@@ -1,36 +1,28 @@
 import React, { useEffect, useState } from 'react'
-import { Container, Button, Form } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap'
 
 import "./viz.css"
 
-const Visualizer = () => {
+const Visualizer = ({ range, input }) => {
     const [array, setArray] = useState([]);
-    const [value , setValue] = useState();
-    const [update, setUpdate] = useState(0);
+  
 
 
     const resetArray = () => {
         let arr = [];
-        for (let i = 0; i <242; i++) {
+        for (let i = 0; i <230; i++) {
             arr.push(randomArray(1, 500))
         }
         setArray(arr)
     }
-    
-    console.log(value)
+
+
 
     const randomArray = (min, max) => {
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 
 
-    /* const handleSlider = (e) =>{
-        let slider = e.target.value ? Math.floor(Math.random()*(500 - 1 + 1)) : 0
-        setValue(slider)
-        console.log(value)
-    }
-       
- */
     const bubbleSort = () => {
 
     }
@@ -43,38 +35,28 @@ const Visualizer = () => {
 
     }
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setUpdate(value);
-        console.log(update);
-        setValue()
-    }
-
 
     useEffect(() => {
         resetArray();
     }, [])
 
     return (
-        <Container className='custom-container'>
-            {array.map((value, idx) => {
-                return (
-                    <div key={idx} className="array-bar" style={{ height: `${value}px` }}>
-                    </div>
-                )
-            })}
+        <Container>
+            <div className='d-flex justify-content-center'>
+                {array.map((value, idx) => {
+                    return (
+                        <div key={idx} className="array-bar" style={{ height: `${value}px` }}>
+                        </div>
+                    )
+                })}
+            </div>
             <Container>
-                <div className="d-flex mt-3">
+                <div className="d-flex mt-3 justify-content-center">
                     <Button variant="warning" className="me-5" onClick={() => { resetArray() }}>  New Array</Button>
                     <Button variant="warning" className="me-5" onClick={() => { bubbleSort() }}>Bubble Sort</Button>
                     <Button variant="warning" className="me-5" onClick={() => { insertionSort() }}>Insertion Sort</Button>
                     <Button variant="warning" className="me-5" onClick={() => { mergeSort() }}>Merge Sort</Button>
-                    <Form>
-                        <div className='d-flex'>
-                            <input type="text" className="me-5" onChange={(event) => setValue(event.target.value)}/>
-                            <Button variant="success" type="submit" className="me-5" onClick={handleSubmit}>Submit</Button>
-                        </div>
-                    </Form>
+                    <Button variant="warning" className="me-5" onClick={range}>Define Range</Button>
                 </div>
             </Container>
         </Container>
